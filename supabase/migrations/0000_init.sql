11f930fe2bf29a02f234e45af548ee620f64e535
Baja Healing <bajahealing@gmail.com>
2025-08-12 23:28:06 +0000

    ok, nice, now let's start to build the app with the stories, please proc

---
+++ supabase/migrations/0000_init.sql
@@ -0,0 +1,35 @@
+-- Create profiles table
+create table profiles (
+  id uuid references auth.users not null primary key,
+  updated_at timestamptz,
+  primary_language text,
+  secondary_language text
+);
+
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/auth/row-level-security
+alter table profiles
+  enable row level security;
+
+create policy "Public profiles are viewable by everyone." on profiles
+  for select using (true);
+
+create policy "Users can insert their own profile." on profiles
+  for insert with check (auth.uid() = id);
+
+create policy "Users can update own profile." on profiles
+  for update using (auth.uid() = id);
+
+-- This trigger automatically creates a profile for new users.
+-- See https://supabase.com/docs/guides/auth/managing-user-data#using-triggers
+create function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.profiles (id)
+  values (new.id);
+  return new;
+end;
+$$ language plpgsql security definer;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
