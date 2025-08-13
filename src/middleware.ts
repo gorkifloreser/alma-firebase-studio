5e163a93c17c5fb76891daca6128661111678066
Baja Healing <bajahealing@gmail.com>
2025-08-13 05:09:27 +0000

    please, deactivate temporarly the middleware, so ican see the developmen

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
-import { type NextRequest } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  // Bypassing authentication for development
+  // return await updateSession(request)
+  return NextResponse.next();
 }
 
 export const config = {
