750ac8e15540e9706d432583fe710b9424d5a84c
Baja Healing <bajahealing@gmail.com>
2025-08-13 01:06:23 +0000

    same error, search online for the docs

--- supabase/migrations/20240815000000_create_profiles_table.sql
+++ supabase/migrations/20240815000000_create_profiles_table.sql
@@ -1,39 +1,53 @@
-
--- Create the profiles table
-create table public.profiles (
-  id uuid not null references auth.users on delete cascade,
+-- Create a table for public profiles
+create table profiles (
+  id uuid references auth.users on delete cascade not null primary key,
+  updated_at timestamp with time zone,
+  username text unique,
+  full_name text,
+  avatar_url text,
+  website text,
   primary_language text,
   secondary_language text,
-  primary key (id)
-);
 
--- Set up Row Level Security
-alter table public.profiles
+  constraint username_length check (char_length(username) >= 3)
+);
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/auth/row-level-security
+alter table profiles
   enable row level security;
 
-create policy "Public profiles are viewable by everyone." on public.profiles
+create policy "Public profiles are viewable by everyone." on profiles
   for select using (true);
 
-create policy "Users can insert their own profile." on public.profiles
-  for insert with check ((select auth.uid()) = id);
+create policy "Users can insert their own profile." on profiles
+  for insert with check (auth.uid() = id);
 
-create policy "Users can update own profile." on public.profiles
-  for update using ((select auth.uid()) = id);
+create policy "Users can update own profile." on profiles
+  for update using (auth.uid() = id);
 
--- This trigger automatically creates a profile for new users
+-- This trigger automatically creates a profile for new users.
+-- See https://supabase.com/docs/guides/auth/managing-user-data#using-triggers for more details.
 create function public.handle_new_user()
-returns trigger
-language plpgsql
-security definer set search_path = public
-as $$
+returns trigger as $$
 begin
   insert into public.profiles (id)
   values (new.id);
   return new;
 end;
-$$;
-
--- Create the trigger
+$$ language plpgsql security definer;
 create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
+
+-- Set up Storage!
+insert into storage.buckets (id, name, public)
+  values ('avatars', 'avatars', true);
+
+create policy "Avatar images are publicly accessible." on storage.objects
+  for select using (bucket_id = 'avatars');
+
+create policy "Anyone can upload an avatar." on storage.objects
+  for insert with check (bucket_id = 'avatars');
+
+create policy "Anyone can update their own avatar." on storage.objects
+  for update using ( auth.uid() = owner ) with check ( bucket_id = 'avatars' );
