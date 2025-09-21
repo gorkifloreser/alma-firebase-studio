2b4cc74acac5bf12831720fb1f2417c0043e4867
Baja Healing <bajahealing@gmail.com>
2025-09-21 18:46:58 +0000

    add spanish and the top ten most common languages to the select input:

--- src/lib/languages.ts
+++ src/lib/languages.ts
@@ -2,8 +2,16 @@
 export const languages = [
     { value: 'en', label: 'English' },
     { value: 'es', label: 'Spanish' },
+    { value: 'zh', label: 'Chinese (Mandarin)' },
+    { value: 'hi', label: 'Hindi' },
     { value: 'fr', label: 'French' },
-    { value: 'de', label: 'German' },
-    { value: 'it', label: 'Italian' },
+    { value: 'ar', label: 'Arabic' },
+    { value: 'bn', label: 'Bengali' },
+    { value: 'ru', label: 'Russian' },
     { value: 'pt', label: 'Portuguese' },
+    { value: 'ur', label: 'Urdu' },
+    { value: 'id', label: 'Indonesian' },
+    { value: 'de', label: 'German' },
+    { value: 'ja', label: 'Japanese' },
+    { value: 'it', label: 'Italian' },
 ];
